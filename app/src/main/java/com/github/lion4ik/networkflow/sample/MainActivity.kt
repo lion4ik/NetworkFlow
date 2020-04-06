63b04db37d4a863fdab38e2534148b6d65fb93eb
Alexey Pushkarev <alexey_pushkarev1@epam.com>
2020-04-06 23:36:02 +0300

    initial project commit

---
+++ app/src/main/java/com/github/lion4ik/networkflow/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.lion4ik.networkflow.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ networkflow/src/main/java/com/github/lion4ik/networkflow/NetworkFlowChecker.kt
@@ -0,0 +1,5 @@
+package com.github.lion4ik.networkflow
+
+class NetworkFlowChecker {
+
+}
